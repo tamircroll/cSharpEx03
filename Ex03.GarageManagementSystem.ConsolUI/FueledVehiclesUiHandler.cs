94c83403a64f08413e10d084337277fc56ba9b0d
tamircroll <tamircroll@gmail>
2015-05-28 02:55:56 +0300

    Extract method

--- Ex03.GarageManagementSystem.ConsolUI/FueledVehiclesUiHandler.cs
+++ Ex03.GarageManagementSystem.ConsolUI/FueledVehiclesUiHandler.cs
@@ -8,46 +8,17 @@ namespace Ex03.GarageManagementSystem.ConsolUI
     {
         public static void UiFuelUp(GarageLogicHandler i_Logic)
         {
-            string plateNumber;
             float mountToFill;
-            Fueled.eFuelType fuelType;
 
             while (true)
             {
-                int fuelTypeIndex = 1;
-
-                Console.Clear();
-                Console.WriteLine("Please enter required plate numer");
-                plateNumber = Console.ReadLine();
                 try
                 {
-                    Array allFuelTypes = i_Logic.AllFuelTypes();
-                    Console.WriteLine("Please enter required fuel type");
-                    foreach (Fueled.eFuelType type in allFuelTypes)
-                    {
-                        Console.WriteLine("{0}. {1}", fuelTypeIndex++, type);
-                    }
-
-                    int fuelTypeValue;
-                    string fuelTypeStr = Console.ReadLine();
-                    bool isNumber = int.TryParse(fuelTypeStr, out fuelTypeValue);
-                    
-                    if (!isNumber)
-                    {
-                        throw new FormatException("The Entered fuel type value is not a number");
-                    }
-
-                    fuelType = i_Logic.IntToFuelType(fuelTypeValue);
-
-                    Console.WriteLine("Please enter how many liters of fuel you would like to fill:");
-                    string litersToFillStr = Console.ReadLine();
-
-                    isNumber = float.TryParse(litersToFillStr, out mountToFill);
-                    if (!isNumber)
-                    {
-                        throw new ArgumentException("The Entered mount of liters is not a number");
-                    }
-
+                    Console.Clear();
+                    Console.WriteLine("Please enter required plate numer");
+                    string plateNumber = Console.ReadLine();
+                    var fuelType = getFuelTypeFromUser(i_Logic);
+                    mountToFill = getMountToFillFromUser();
                     i_Logic.FillFuel(plateNumber, mountToFill, fuelType);
                     Console.WriteLine("Fuel was successfully filled");
                     Console.ReadLine();
@@ -62,6 +33,42 @@ namespace Ex03.GarageManagementSystem.ConsolUI
                     }
                 }
             }
+        }
+
+        private static float getMountToFillFromUser()
+        {
+            float mountToFill;
+            Console.WriteLine("Please enter how many liters of fuel you would like to fill:");
+            string litersToFillStr = Console.ReadLine();
+            bool isNumber = float.TryParse(litersToFillStr, out mountToFill);
+            if (!isNumber)
+            {
+                throw new ArgumentException("The Entered mount of liters is not a number");
+            }
+
+            return mountToFill;
+        }
+
+        private static Fueled.eFuelType getFuelTypeFromUser(GarageLogicHandler i_Logic)
+        {
+            Array allFuelTypes = i_Logic.AllFuelTypes();
+            Console.WriteLine("Please enter required fuel type");
+            foreach (Fueled.eFuelType curFuelType in allFuelTypes)
+            {
+                Console.WriteLine("{0}. {1}", (int) curFuelType, curFuelType);
+            }
+
+            int fuelTypeValue;
+            string fuelTypeStr = Console.ReadLine();
+            bool isNumber = int.TryParse(fuelTypeStr, out fuelTypeValue);
+
+            if (!isNumber)
+            {
+                throw new FormatException("The Entered fuel type value is not a number");
+            }
+
+            Fueled.eFuelType fuelType = i_Logic.IntToFuelType(fuelTypeValue);
+            return fuelType;
         }
     }
 }
